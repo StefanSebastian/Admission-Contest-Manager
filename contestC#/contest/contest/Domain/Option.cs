50867702717d7f456e75f009cc5703f37c9b4a05
StefanSebastian <stefansebii@gmail.com>
2016-12-15 14:51:47 +0200

    Added Option entity

---
+++ contestC#/contest/contest/Domain/Option.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contest.Domain
+{
+    public class Option
+    {
+        //attributes
+        Tuple<int, int> id;
+        private Candidate candidate;
+        private Department department;
+
+        //getters and setters
+        public Candidate Candidate
+        {
+            get
+            {
+                return candidate;
+            }
+
+            set
+            {
+                candidate = value;
+            }
+        }
+
+        public Department Department
+        {
+            get
+            {
+                return department;
+            }
+
+            set
+            {
+                department = value;
+            }
+        }
+
+        public Tuple<int, int> Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        //Constructor 
+        public Option(Candidate candidate, Department department)
+        {
+            this.candidate = candidate;
+            this.department = department;
+            this.id = new Tuple<int, int>(candidate.Id, department.Id);
+        }
+
+        //To string method 
+        public override string ToString()
+        {
+            return "Candidate id " + id.Item1 + " with option id " + id.Item2;
+        }
+
+        //Equals method 
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Option))
+            {
+                return false;
+            }
+            Option o = (Option)obj;
+            return id.Equals(o.id);
+        }
+    }
+}
---
+++ contestC#/contest/contestTests/Domain/OptionTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using contest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contest.Domain.Tests
+{
+    [TestClass()]
+    public class OptionTests
+    {
+        [TestMethod()]
+        public void OptionTest()
+        {
+            Department d = new Department(1, "department", 120);
+            Candidate c = new Candidate(1, "Name", "0742345233", "address 1/23");
+            Option o = new Option(c, d);
+            Assert.AreEqual(o.Id.Item1, c.Id);
+            Assert.AreEqual(o.Id.Item2, d.Id);
+            Assert.AreEqual(o.Candidate, c);
+            Assert.AreEqual(o.Department, d);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Department d = new Department(1, "department", 120);
+            Candidate c = new Candidate(1, "Name", "0742345233", "address 1/23");
+            Option o = new Option(c, d);
+            Assert.AreEqual(o.ToString(), "Candidate id 1 with option id 1");
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Department d = new Department(1, "department", 120);
+            Department d2 = new Department(12, "department2", 500);
+            Candidate c = new Candidate(1, "Name", "0742345233", "address 1/23");
+            Option o = new Option(c, d);
+            Option o2 = new Option(c, d);
+            Option o3 = new Option(c, d2);
+            Assert.AreEqual(o, o2);
+            Assert.AreNotEqual(o, o3);
+            Assert.AreNotEqual(o2, o3);
+        }
+    }
+}
